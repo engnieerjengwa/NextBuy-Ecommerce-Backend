9c6c1d96ccb2b2846580b5dd02c9807fe2edb18d
Phillip Jengwa <PhillipJ@vertice.software>
2026-01-08 15:57:57 +0200

    Add CrossOrigin annotations for repositories
    Enable Cross-Origin Resource Sharing (CORS) for `localhost:4200` on all specified repository interfaces. This change facilitates front-end access during development by allowing requests from the Angular application.

--- src/main/java/com/ecommerce/NexBuy/dao/ProductCategoryRepository.java
+++ src/main/java/com/ecommerce/NexBuy/dao/ProductCategoryRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(path = "product-category", collectionResourceRel = "productCategory")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
--- src/main/java/com/ecommerce/NexBuy/dao/ProvinceRepository.java
+++ src/main/java/com/ecommerce/NexBuy/dao/ProvinceRepository.java
@@ -4,8 +4,10 @@ import com.ecommerce.NexBuy.entity.Province;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import java.util.List;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(path = "provinces")
 public interface ProvinceRepository extends JpaRepository<Province, Long> {
     List<Province> findByCountryCode(@Param("code") String code);
